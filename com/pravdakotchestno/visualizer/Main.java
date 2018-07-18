31356b249b2ccae00b491519ebf3909eab8e09a9
pravdakotchestno <pravdakotchestno@protonmail.ch>
2018-07-18 21:49:45 +0700

    Main.java added

---
+++ com/pravdakotchestno/visualizer/Main.java
@@ -0,0 +1,13 @@
+package com.pravdakotchestno.visualizer;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Frame();
+    }
+    public static void paintVisualization(Graphics gr, String address){
+
+    }
+}
