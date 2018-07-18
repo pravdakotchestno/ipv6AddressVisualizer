da97832b189ad575034aac4b95aca8aa56416159
pravdakotchestno <pravdakotchestno@protonmail.ch>
2018-07-18 21:49:55 +0700

    Panel.java added

---
+++ com/pravdakotchestno/visualizer/Panel.java
@@ -0,0 +1,35 @@
+package com.pravdakotchestno.visualizer;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Panel extends JPanel implements MouseListener{
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
