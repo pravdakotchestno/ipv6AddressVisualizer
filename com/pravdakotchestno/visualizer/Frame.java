d65c62ebcdae0ac5fd8b68317e6aaf9714eabb52
pravdakotchestno <pravdakotchestno@protonmail.ch>
2018-07-18 21:49:33 +0700

    Frame.java added

---
+++ com/pravdakotchestno/visualizer/Frame.java
@@ -0,0 +1,14 @@
+package com.pravdakotchestno.visualizer;
+
+import javax.swing.*;
+
+public class Frame extends JFrame{
+    public Frame(){
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300,500);
+        setLocationRelativeTo(null);
+
+    }
+}
