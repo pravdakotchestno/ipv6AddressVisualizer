e821791bcbe8483dd72c457e28101bf3f501be2f
pravdakotchestno <pravdakotchestno@protonmail.ch>
2018-07-20 21:41:43 +0700

    Frame.java updated

--- com/pravdakotchestno/visualizer/Frame.java
+++ com/pravdakotchestno/visualizer/Frame.java
@@ -11,7 +11,15 @@ public class Frame extends JFrame{
         octets = new ArrayList<>();
         for(int i = 0; i<8; i++){
             octets.add(new JTextField());
-            //octets.get(i).setBounds();
+            octets.get(i).setBounds(4*i+45*i+5,10,45,30);
+            add(octets.get(i));
+        }
+
+        for(int i = 0; i < 7; i++){
+            JLabel label = new JLabel();
+            label.setBounds(45*i+4*i+49,10,4,30);
+            label.setText(":");
+            add(label);
         }
 
         panel = new Panel();
@@ -21,7 +29,6 @@ public class Frame extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400,550);
         setLocationRelativeTo(null);
-
         setVisible(true);
 
     }
