d1a681fe55c9fd25d658cdab60261bf014cee300
pravdakotchestno <pravdakotchestno@protonmail.ch>
2018-07-21 00:20:04 +0700

    paintVisualization() method updated

--- com/pravdakotchestno/visualizer/Main.java
+++ com/pravdakotchestno/visualizer/Main.java
@@ -1,6 +1,7 @@
 package com.pravdakotchestno.visualizer;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
 
@@ -8,16 +9,54 @@ public class Main {
         new Frame();
     }
 
-    public static String changeAddress(String address, int x, int y){
-        //TODO
-        return address;
-    }
-
     public static void paintVisualization(Graphics g, String address){
-        //TODO
-        g.setColor(Color.WHITE);
-        g.fillRect(0,130,400,400);
-        g.setColor(Color.BLACK);
-        g.fillRect(0,130,50,50);
+        String octets[] = address.split(":");
+        String oldoctets[] = octets;
+        for(int i = 0; i < 8; i++){
+            String octet = "";
+            for(char ch : oldoctets[i].toCharArray()){
+                int b = (ch - '0') % 4;
+                int a = (ch - '0') >> 2;
+                octet+=(a +""+ b);
+            }
+            octets[i] = octet;
+        }
+
+        try{
+            for(int y = 0; y < 8; y++){
+                for(int x = 0; x < 8; x++){
+                    int currentchar = octets[y].toCharArray()[x]-'0';
+
+                    switch(currentchar) {
+                        case 0: {
+                            g.setColor(Color.BLACK);
+                            break;
+                        }
+                        case 1: {
+                            g.setColor(Color.WHITE);
+                            break;
+                        }
+                        case 2: {
+                            g.setColor(Color.RED);
+                            break;
+                        }
+                        case 3: {
+                            g.setColor(Color.GREEN);
+                            break;
+                        }
+                        default: {
+                            throw new ArithmeticException();
+                        }
+                    }
+                    g.fillRect(x * 50, y * 50 + 130,50,50);
+                }
+
+            }
+        }catch(Exception e){
+            g.setColor(Color.WHITE);
+            g.fillRect(0,130,400,400);
+        }
+
+
     }
 }
